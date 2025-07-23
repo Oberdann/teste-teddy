b6b046ad09f4b9629ba5242fe026dc34d7b63fec
Oberdann <oberdandeveloper@gmail.com>
2025-07-22 21:03:14 -0300

    feat: new dependecy and docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Teste Tecnico Teddy')
+    .setDescription('Software para armazenamento/criação de encurtar de url')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, documentFactory);
+
+  await app.listen(process.env.PORT || 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
